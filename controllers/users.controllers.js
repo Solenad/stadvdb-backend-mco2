2c2dc54fb75677d1a71247d703264f9cc9be4eba
Solenad <roe.dizon80@gmail.com>
2025-11-27 01:15:54 +0800

    feat: added controller for users

--- controllers/users.controllers.js
+++ controllers/users.controllers.js
@@ -2,6 +2,33 @@ import * as UsersService from "../services/users.services.js";
 
 export async function getUsers(req, res) {
   try {
-    const users = await UsersService
+    const users = await UsersService.getAllUsers();
+
+    res.status(200).json(users);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
   }
 }
+
+// GET /users/:id
+export const getUser = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const user = await UsersService.getUserById(id);
+    if (!user) return res.status(404).json({ error: "User not found" });
+    res.status(200).json(user);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+// GET /users/year/:year
+export const getUsersByYear = async (req, res) => {
+  try {
+    const year = parseInt(req.params.year);
+    const users = await UsersService.getAllUsersByDate(year);
+    res.status(200).json(users);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
--- services/users.services.js
+++ services/users.services.js
@@ -4,27 +4,28 @@ export const getAllUsers = async () => {
   const pools = await initPools();
   const node1 = pools.node1;
   const [rows] = await node1.query("SELECT * FROM Users");
-  return rows;
+  return rows || null;
 };
 
 export const getUserById = async (id) => {
   const pools = await initPools();
   const node1 = pools.node1;
   const [rows] = await node1.query("SELECT * FROM Users WHERE id = ?", [id]);
-  return rows.length ? rows[0] : null;
+  return rows[0] || null;
 };
 
 export const getAllUsersByDate = async (year) => {
-  if (year != 2006 || year != 2007) return null;
-
   const pools = await initPools();
   const node2 = pools.node2;
   const node3 = pools.node3;
 
   if (year === 2006) {
     const [rows] = await node2.query("SELECT * FROM Users");
-  } else {
+    return rows;
+  } else if (year === 2007) {
     const [rows] = await node3.query("SELECT * FROM Users");
+    return rows;
+  } else {
+    return null;
   }
-  return rows;
 };
